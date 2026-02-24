4c4a84af8b21bd567eb8592620313352e1133cb3
Keerthana9447 <keerthanagummadi25@gmail.com>
2026-02-24 15:02:10 +0530

    Update register page to match the design of login page.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,40 +1,86 @@
-import { useState } from "react";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Register() {
-    const [fullname, setFullName] = useState("");
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    function handleOnClick() {
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-
-        const raw = JSON.stringify({
-            "fullName": fullname,
-            "username": username,
-            "password": password
-        });
-
-        const requestOptions = {
-            method: "POST",
-            headers: myHeaders,
-            body: raw,
-            redirect: "follow"
-        };
-
-        fetch("http://localhost:8080/register", requestOptions)
-            .then((response) => response.text())
-            .then((result) => console.log(result))
-            .catch((error) => console.error(error));
-    }
-
-    return <>
-        <h1>Register</h1>
-        <input type="text" onChange={(e) => { setFullName(e.target.value) }} placeholder="FullName" />
-        <input type="text" onChange={(e) => { setUsername(e.target.value) }} placeholder="Username" />
-        <input type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password" />
-        <button onClick={handleOnClick}>Submit</button>
-    </>
+    return (
+        <>
+            <div className='flex flex-col items-center gap-3 min-h-screen justify-center'>
+                <h1 className='text-3xl font-bold pb-2'>Register</h1>
+                <label className="input validator">
+                    <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <g
+                            strokeLinejoin="round"
+                            strokeLinecap="round"
+                            strokeWidth="2.5"
+                            fill="none"
+                            stroke="currentColor"
+                        >
+                            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                            <circle cx="12" cy="7" r="4"></circle>
+                        </g>
+                    </svg>
+                    <input
+                        type="text"
+                        required
+                        placeholder="Full Name"
+                        pattern="[A-Za-z][A-Za-z0-9\-]*"
+                        minLength="3"
+                        maxLength="30"
+                        title="Only letters, numbers or dash"
+                    />
+                </label>
+                <label className="input validator">
+                    <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <g
+                            strokeLinejoin="round"
+                            strokeLinecap="round"
+                            strokeWidth="2.5"
+                            fill="none"
+                            stroke="currentColor"
+                        >
+                            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                            <circle cx="12" cy="7" r="4"></circle>
+                        </g>
+                    </svg>
+                    <input
+                        type="text"
+                        required
+                        placeholder="Username"
+                        pattern="[A-Za-z][A-Za-z0-9\-]*"
+                        minLength="3"
+                        maxLength="30"
+                        title="Only letters, numbers or dash"
+                    />
+                </label>
+                <label className="input validator">
+                    <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <g
+                            strokeLinejoin="round"
+                            strokeLinecap="round"
+                            strokeWidth="2.5"
+                            fill="none"
+                            stroke="currentColor"
+                        >
+                            <path
+                                d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
+                            ></path>
+                            <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                        </g>
+                    </svg>
+                    <input
+                        type="password"
+                        required
+                        placeholder="Password"
+                        minLength="8"
+                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                        title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                    />
+                </label>
+                <button className="btn btn-neutral w-80">Login</button>
+                <p>Already have an account? <Link className='link' to="/login">Login</Link></p>
+            </div>
+        </>
+    )
 }
 
-export default Register;
+export default Register
