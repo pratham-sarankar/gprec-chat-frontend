7c72e1cab870fe167638cce77bfc072752af905c
Keerthana9447 <keerthanagummadi25@gmail.com>
2026-02-24 14:37:44 +0530

    Add react router dom & update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import Register from './pages/Register.jsx'
 import Login from './pages/Login.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login></Login>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
